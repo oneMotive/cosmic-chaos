75058eb553afe4ce350773fda31df7a33b4880fc
Mike Stewart <dungeonmesher@protonmail.com>
2025-11-13 23:01:06 -0500

    Full cosmic setup + steam

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 -y install @cosmic-desktop-environment --skip-unavailable
+dnf5 -y install @cosmic-desktop-environment \
+    steam
 
 # Use a COPR Example:
 #
